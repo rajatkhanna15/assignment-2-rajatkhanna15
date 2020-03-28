4fdcaef12cf6696f902b0b966ed1fb354a30654c
rajatkhanna15 <rajat.khanna_cs18@gla.ac.in>
2020-03-28 17:30:53 +0530

    printing the preOrder traversal.

--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -11,6 +11,31 @@ package problem2.main;
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 
 
+import problem1.mybst.MyBinarySearchTree;
+
 public class MyMain {
 
+    public static void main(String[] args) {
+        /*
+        Creating object of MyBinarySearchTree class of problem 1 to create binary tree.
+         */
+        MyBinarySearchTree mbst = new MyBinarySearchTree();
+        /*
+        inserting values in the tree.
+         */
+        mbst.insert(2);
+        mbst.insert(10);
+        mbst.insert(5);
+        mbst.insert(7);
+        mbst.insert(27);
+        mbst.insert(59);
+        mbst.insert(40);
+        mbst.insert(25);
+        mbst.insert(7);
+        mbst.insert(11);
+        /*
+        printing the required result.
+         */
+
+    }
 }
