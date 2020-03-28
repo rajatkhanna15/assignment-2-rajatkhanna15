8ef9c0f12a7e76cdd16ac5c89c190a6689dcbcb3
rajatkhanna15 <rajat.khanna_cs18@gla.ac.in>
2020-03-28 15:52:56 +0530

    end of inser() method.

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -24,5 +24,59 @@ public class MyBinarySearchTree {
         root = null;
     }
 
+    /*
+    insert method to insert a node
+     */
+    public void insert(int value)
+    {
+        /*
+        creating object of TreeNode class.
+         */
+        TreeNode newNode = new TreeNode();
+        newNode.setData(value);
+
+        /*
+        checking if tree is empty.
+         */
+        if(root == null)
+        {
+            root = newNode;
+        }
+        else
+        {
+            TreeNode current = root;
+            TreeNode parent;
+            while(true)
+            {
+                parent = current;
+                /*
+                if the value to be inserted is less than the root then value is
+                inserted at left of the root otherwise at the right.
+                 */
+                if(value < current.getData())
+                {
+                    current = current.getLeft();
+                    if(current == null)
+                    {
+                        parent.setLeft(newNode);
+                        return;
+                    }
+                }
+                else
+                {
+                    current = current.getRight();
+                    if(current == null)
+                    {
+                        parent.setRight(newNode);
+                        return;
+                    }
+                }
+            }
+        }
+    }
+
+
+
+
 
 }
