5e12e624091abee4f1780b7556d355bd09554c13
rajatkhanna15 <rajat.khanna_cs18@gla.ac.in>
2020-03-28 17:17:07 +0530

    printing the left child nodes.

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -8,9 +8,34 @@ package problem1.main;
 // executable class
 
 
+import problem1.mybst.MyBinarySearchTree;
+
 public class MyMain {
 
     public static void main(String[] args) {
+        /*
+        Creating object of MyBinarySearchTree class.
+         */
+        MyBinarySearchTree mbst = new MyBinarySearchTree();
+        /*
+        inserting values
+         */
+        mbst.insert(22);
+        mbst.insert(18);
+        mbst.insert(65);
+        mbst.insert(87);
+        mbst.insert(23);
+        mbst.insert(19);
+        mbst.insert(60);
+        mbst.insert(55);
+        mbst.insert(27);
+        mbst.insert(1);
+
+        /*
+        Printing the required result.
+         */
+
+
 
     }
 }
