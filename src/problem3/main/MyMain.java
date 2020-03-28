3366eb91b0266a463fba30373d75386295803a31
rajatkhanna15 <rajat.khanna_cs18@gla.ac.in>
2020-03-28 18:20:56 +0530

    printing the students in ascending order according to roll no.

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -6,8 +6,27 @@
  */
 
 package problem3.main;
+
+import problem3.myqueue.MyPriorityQueue;
+
 // executable class
 // use problem5.student.Student class to create object of student
 public class MyMain {
 
+    public static void main(String[] args) {
+        /*
+        object creation ofMyPriorityQueue class.
+         */
+        MyPriorityQueue mpq = new MyPriorityQueue();
+        /*
+        inserting values
+         */
+        mpq.insert("Yash",45);
+        mpq.insert("Yash",45);
+        mpq.insert("Yash",45);
+        mpq.insert("Yash",45);
+        mpq.insert("Yash",45);
+
+
+    }
 }
