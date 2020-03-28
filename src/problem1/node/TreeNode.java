1b1003193991d63b18fc6a310ed57b8f78082b19
rajatkhanna15 <rajat.khanna_cs18@gla.ac.in>
2020-03-28 14:55:40 +0530

    getData() method to read the value of data.

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -8,5 +8,38 @@ package problem1.node;
 
 
 public class TreeNode {
+    /*
+    Variables to be used
+     */
+    private int data;
+    private TreeNode right;
+    private TreeNode left;
 
+    /*
+    Getter and setter methods for the variables.
+     */
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public TreeNode getRight() {
+        return right;
+    }
+
+    public void setRight(TreeNode right) {
+        this.right = right;
+    }
+
+    public TreeNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(TreeNode left) {
+        this.left = left;
+    }
 }
