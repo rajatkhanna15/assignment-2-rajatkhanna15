9cc081ef261d887707bd02eb3b56e105da15a982
rajatkhanna15 <rajat.khanna_cs18@gla.ac.in>
2020-03-28 22:42:30 +0530

    getData() method starts.

--- src/problem4/node/Node.java
+++ src/problem4/node/Node.java
@@ -3,4 +3,30 @@ package problem4.node;
 
 
 public class Node {
+    /*
+    Some variables to be used
+     */
+    int data;
+    Node next;
+
+    /*
+    getter and setter methods for variables.
+     */
+
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
 }
