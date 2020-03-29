3ab70ab810b448bd8da3c89e48653397ac3814a2
rajatkhanna15 <rajat.khanna_cs18@gla.ac.in>
2020-03-29 14:25:02 +0530

    main() method ends.

--- src/problem5/main/MyMain.java
+++ src/problem5/main/MyMain.java
@@ -6,7 +6,32 @@
  */
 
 package problem5.main;
+
+import problem5.circularqueue.MyCircularQueue;
+
 //executable class
 public class MyMain {
 
+    public static void main(String[] args) {
+        /*
+        Creating object of MyCircularQueue class
+         */
+        MyCircularQueue mcq = new MyCircularQueue();
+        /*
+        inserting values
+         */
+        mcq.enqueue(1,4,0);
+        mcq.enqueue(2,2,1);
+        mcq.enqueue(3,3,2);
+        mcq.enqueue(4,1,2);
+        mcq.enqueue(5,0,1);
+        mcq.enqueue(6,2,0);
+        mcq.enqueue(7,0,2);
+        mcq.enqueue(8,1,1);
+        mcq.enqueue(9,0,2);
+        mcq.enqueue(10,0,0);
+
+
+
+    }
 }
