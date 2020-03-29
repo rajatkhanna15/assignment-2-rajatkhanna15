8369fdfa5671c634de8a4a3353e4e3d6106d220c
rajatkhanna15 <rajat.khanna_cs18@gla.ac.in>
2020-03-29 12:20:50 +0530

    main() method ends.

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -6,6 +6,10 @@
  */
 
 package problem4.main;
+
+import problem1.mybst.MyBinarySearchTree;
+import problem4.myqueue.MyQueue;
+
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
@@ -17,6 +21,32 @@ public class MyMain {
         /*
         Creating object of MyBinarySearchTree class of problem 1.
          */
+        MyBinarySearchTree mbst = new MyBinarySearchTree();
+        /*
+        inserting values in tree.
+         */
+        mbst.insert(10);
+        mbst.insert(20);
+        mbst.insert(40);
+        mbst.insert(17);
+        mbst.insert(48);
+        mbst.insert(21);
+        mbst.insert(11);
+        mbst.insert(98);
+        mbst.insert(4);
+        mbst.insert(1);
+
+        /*
+        creation of object of MyQueue class
+         */
+        MyQueue mq = new MyQueue();
+        mq.preOrderSuccessor(mbst.root);
+        /*
+        Displaying the result
+         */
+        System.out.println("Pre Order Successor of all the nodes are :");
+        mq.dispPreOrderSuccessor();
 
     }
+
 }
